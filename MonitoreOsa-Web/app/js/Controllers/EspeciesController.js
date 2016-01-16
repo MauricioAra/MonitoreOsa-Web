5276e85076594d7d93dd602539d546f120508035
mauricioara <mauricioher.94@hotmail.com>
2016-01-16 13:53:58 -0600

    Eliminar y mostrar especies

--- MonitoreOsa-Web/app/js/Controllers/EspeciesController.js
+++ MonitoreOsa-Web/app/js/Controllers/EspeciesController.js
@@ -1,31 +1,27 @@
-App.controller('EspeciesCtrl',function($state,$scope, $http, Upload){
+App.controller('EspeciesCtrl',function($state,$scope, $http, Upload,EspecieService){
   $scope.currentPage = 0;
   $scope.pageSize = 5;
   especie = {};
-  $scope.listaEspecies = [];
+  $scope.listaEspecies = EspecieService.list();
   $scope.files = {};
   $scope.especie = {}
   $scope.especie.imagen = {};
   var file;
   var imagenBase64 = {};
-
+  var id;
+  var rev;
+  
   $scope.uploader = {};
 
-  $http.get("https://mmullerc.cloudant.com/mamiferos/_all_docs?&include_docs=true").then(function(response) {
-    for(var i = 0; i < response.data.rows.length; i++){
-        especie = response.data.rows[i].doc;
-        $scope.listaEspecies[i] = especie;
-      }
-  });
-
+  //
   $scope.numberOfPages=function(){
     return Math.ceil($scope.listaEspecies.length/$scope.pageSize);
   }
-
+  //
   $scope.registrar = function(){
     $('#modal1').openModal();
   }
-
+  //
   $scope.onFileSelect = function ($files) {
 
 
@@ -34,7 +30,7 @@ App.controller('EspeciesCtrl',function($state,$scope, $http, Upload){
            file = $scope.files[i];
           }
   }
-
+  //
   $scope.convertirImagen = function(){
     $scope.processFiles = function(files){
       angular.forEach(files, function(flowFile, i){
@@ -47,9 +43,33 @@ App.controller('EspeciesCtrl',function($state,$scope, $http, Upload){
       });
     };
   }
-
+  //Eliminar especie
+  $scope.eliminar = function(especie){
+       id = especie._id;
+       rev = especie._rev;
+       $('#confirmacion').openModal();
+  }
+  //Si
+  $scope.si = function(){
+      $http.delete("https://mmullerc.cloudant.com/especies/"+id+"?rev="+rev+"").then(function(response) {
+        $('#confirmacion').closeModal();
+        $scope.listaEspecies = EspecieService.list();
+    });
+  }
+  //No
+  $scope.no = function(){
+      $('#confirmacion').closeModal();
+  }
+  //Ver especie
+  $scope.verEspecie = function(especie){
+      $('#informacion').openModal();
+      $scope.nombreModal = especie.nombre;
+      $scope.cientificoModal = especie.nombre_cientifico;
+      $scope.descripcionModal = especie.descripcion;
+      $scope.tipoModal =  especie.tipo;
+  }
+  //Registrar especie
   $scope.aceptarRegistro = function(){
-
     var nombre = document.getElementById("nombre_especie").value;
     var nombre_cientifico = document.getElementById("nombre_cientifico").value;
     var descripcion = document.getElementById("descripcion").value;
@@ -73,8 +93,9 @@ App.controller('EspeciesCtrl',function($state,$scope, $http, Upload){
         }
       }
     }
-    $http.post("https://mmullerc.cloudant.com/mamiferos/", objRegistro).then(function(response) {
-      console.log(response);
+    $http.post("https://mmullerc.cloudant.com/especies/", objRegistro).then(function(response) {
+        $('#registrado').openModal();
+        $scope.listaEspecies = EspecieService.list();
     });
   }
 
--- MonitoreOsa-Web/app/js/Services/EspecieService.js
+++ MonitoreOsa-Web/app/js/Services/EspecieService.js
@@ -2,16 +2,14 @@ App.service('EspecieService',function($http){
 
   this.list = function () {
     especie = {};
-    listaEspecies = {};
+    listaEspecies = [];
 
-    $http.get("https://mmullerc.cloudant.com/mamiferos/_all_docs?&include_docs=true").then(function(response) {
-
-      for(var i = 0; i < response.data.rows.length; i++){
-          especie = response.data.rows[i].doc;
-          $scope.listaEspecies[i] = especie;
+    $http.get("https://mmullerc.cloudant.com/especies/_all_docs?&include_docs=true").then(function(response) {
+        for(var i = 0; i < response.data.rows.length; i++){
+            especie = response.data.rows[i].doc;
+            listaEspecies[i] = especie;
         }
-        console.log($scope.listaEspecies);
-
     });
-
+    return listaEspecies;
+  }
 });
